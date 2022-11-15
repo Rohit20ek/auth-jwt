93870b91cbd04e554295fa255a73ea59655d390d
Rohit20ek <kr1998rohitk@gmail.com>
2022-11-15 08:57:22 +0530

    pract1

---
+++ authServer.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+let refreshTokens = []
+
+app.post('/token', (req,res) => {
+    const refreshToken = req.body.refreshToken
+    
+})
